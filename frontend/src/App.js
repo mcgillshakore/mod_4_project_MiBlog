e2a823432ef22f75ef4e5f2217e93be85bbd6e17
github username <github email address>
2020-08-18 16:32:44 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BlogForm from './BlogForm'
 import SearchBox from './SearchBox'
 import BlogList from './BlogList'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
  import './App.css';
@@ -9,25 +10,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class App extends React.Component{
 
   state ={
-    blogs: []
+    blogs: [],
+    comments: []
   }
 
-  componentDidMount () {
+
+  fetchBlogs = () => {
     fetch('http://localhost:3000/blogs')
     .then(res => res.json())
     .then( blogs => this.setState({
       blogs: blogs
     }))
+  } 
+
+  fetchComments = () => {
+    fetch('http://localhost:3000/comments')
+     .then(res => res.json())
+     .then(comments => this.setState({
+       comments: comments
+     }))
   }
 
+  componentDidMount () {
+    this.fetchBlogs ()
+     this.fetchComments()
+   }
+ 
   render() {
     return (
-      <div className="App">
-   <BlogForm />
+      <div className="app">
+        <div className="sidebar">
+          <button>Show/hide new poem form</button>
+          {false && <BlogForm />}
+        </div>
         <SearchBox />
-        <BlogList  blogs={this.state.blogs} />
-      
-     
+        <BlogList blogs={this.state.blogs} />
+       
       </div>
     );
   }
