cc4c7cf531e8ca0b6d2f499b8328f4124be9421e
mcgillshakore <mcgills2093@gmail.com>
2020-08-20 10:38:54 -0500

    updated backend v3.1

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -3,9 +3,7 @@ class AuthController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
         if user != nil && user.authenticate(params[:password])
-            # session[:user_id] = user.id
-            # byebug
-            current_user = {username: user.username, email: user.email, id: user.id}
+            current_user = {username: user.username, id: user.id}
             render json: current_user
         else
             render json: {error: 'Invalid User'}
